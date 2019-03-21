7fdc4276a3482cf63c8c882e346488598f681ad2
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2019-03-22 01:40:01 +0200

    Working implementation with symbols table

--- program.py
+++ program.py
@@ -2,7 +2,7 @@ import sys
 import os
 from parser_module import Parser
 from code_module import Code
-from predefined_symbols import predefined_symbols
+from symbols_module import SymbolTable
 
 
 def main():
@@ -17,33 +17,80 @@ def main():
         print('The file {} does not exists.'.format(input_filename))
         return
 
-    p = Parser(input_filename)
+    # parse labels
+    st = SymbolTable()
+
+    p0 = Parser(input_filename)
+    position_address = 0
+
+    while True:
+        if not p0.has_more_commands():
+            break
+
+        p0.advance()
+        command_type = p0.command_type()
+        symbol = p0.symbol()
+        position_address += 1
+
+        if command_type == 'L_COMMAND':
+            position_address -= 1
+            if not st.contains(symbol):
+                st.add_entry(symbol, position_address)
+
+    # parse addresses
+    p1 = Parser(input_filename)
+    variable_address = 16
+
+    while True:
+        if not p1.has_more_commands():
+            break
+
+        p1.advance()
+        command_type = p1.command_type()
+        symbol = p1.symbol()
+
+        if command_type == 'A_COMMAND':
+            if not st.contains(symbol) and not is_number(symbol):
+                st.add_entry(symbol, variable_address)
+                variable_address += 1
+
+    # second pass
+    p2 = Parser(input_filename)
     with open(output_filename, 'w') as fout:
         while True:
-            if not p.has_more_commands():
+            if not p2.has_more_commands():
                 break
 
-            p.advance()
-            command = decode_command(p)
-            fout.write(command + '\n')
+            p2.advance()
+            command = decode_command(p2, st)
+            if command:
+                fout.write(command + '\n')
 
 
 def decimal_to_binary_str(dec_value):
     return format(dec_value, '015b')
 
 
-def decode_command(p):
+def is_number(string):
+    try:
+        int(string)
+        return True
+    except:
+        return False
+
+
+def decode_command(p, st):
     command_type = p.command_type()
     command_decoded = ''
     c = Code()
 
     if command_type == 'L_COMMAND':
-        command_decoded = p.symbol()
+        command_decoded = None
     elif command_type == 'A_COMMAND':
         symbol = p.symbol()
-        if symbol in predefined_symbols:
-            predefined_symbol = predefined_symbols[symbol]
-            command_decoded = '0' + decimal_to_binary_str(predefined_symbol)
+        if st.contains(symbol):
+            symbol_value = st.get_address(symbol)
+            command_decoded = '0' + decimal_to_binary_str(symbol_value)
         else:
             command_decoded = '0' + decimal_to_binary_str(int(symbol))
 
